312a9ec6f1d4ed545e0cb8c64f4aa736cdcfd7a8
Md Shahin Howlader <mdshahin0002@gmail.com>
2021-04-21 07:33:42 +0200

    project structure change

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/")
 public class HelloController {
-    @RequestMapping("/")
+    @RequestMapping(path = "/")
     public String home() {
         return "This is home page";
     }
-    @RequestMapping("/hello")
+
+    @RequestMapping(path = "hello")
     public String sayHi() {
         return "Hi";
     }
