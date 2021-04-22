87fdc3f0e16df29c3ad07c72ef21343251eaefde
Md Shahin Howlader <mdshahin0002@gmail.com>
2021-04-22 16:30:51 +0200

    add resource using post method done

--- src/main/java/com/example/topic/TopicService.java
+++ src/main/java/com/example/topic/TopicService.java
@@ -2,18 +2,19 @@ package com.example.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "Spring Framework", "Spring Framework Description"),
             new Topic("Java", "Java Framework", "Java Framework Description"),
             new Topic("JavaScript", "JavaScript Framework", "JavaScript Framework Description"),
             new Topic("php", "Spring Framework", "Spring Framework Description"),
             new Topic("laravel", "Spring Framework", "Spring Framework Description")
-    );
+    ));
 
     public List<Topic> getAllTopics() {
         return topics;
@@ -21,5 +22,8 @@ public class TopicService {
 
     public Topic getTopic(String id) {
         return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    }
+    public void addTopic(Topic topic) {
+        topics.add(topic);
     }
 }
