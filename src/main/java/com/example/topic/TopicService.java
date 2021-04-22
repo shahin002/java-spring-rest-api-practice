51cde16cf9282d80b1e3bf79ede4cef0ee317c2a
Md Shahin Howlader <mdshahin0002@gmail.com>
2021-04-22 07:57:15 +0200

    getting a single resource

--- src/main/java/com/example/topic/TopicService.java
+++ src/main/java/com/example/topic/TopicService.java
@@ -18,4 +18,8 @@ public class TopicService {
     public List<Topic> getAllTopics() {
         return topics;
     }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    }
 }
